4342a10311844b028f1bf41111217e3fc22ac85e
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-03-26 15:31:54 +0900

    utf 설정 등 번거로움을 느껴보자

--- src/main/java/com/example/lms/MainPage.java
+++ src/main/java/com/example/lms/MainPage.java
@@ -1,22 +1,40 @@
 package com.example.lms;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
 public class MainPage {
-    @GetMapping("/")
-    public String index(){
-        return "Index Page";
+    @RequestMapping("/")
+    public void index(HttpServletRequest request,
+                        HttpServletResponse response) throws IOException {
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write("Index Page");
+        printWriter.close();
     }
-    @GetMapping("/hello")
-    public String hello(){
-        return "<html>" +
+    @RequestMapping("/hello")
+    public void hello(HttpServletRequest request,
+                        HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter printWriter = response.getWriter();
+
+        String msg = "<html>" +
                 "<head>" +
+                "<meta charset=\"UTF-8\">" +
                 "</head>" +
                 "<body>" +
-                "<p>hello</p> <p> fastlms website!!!</p>" +
+                "<p>hello</p> " +
+                "<p> fastlms website!!!</p>" +
+                "<p> 안녕하세요</p>" +
                 "</body>" +
                 "</html>";
+
+        printWriter.write(msg);
+        printWriter.close();
     }
 }
